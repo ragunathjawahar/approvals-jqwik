39be909b0f8f4aa76095219e6dbe4f38c03fefbb
Ragunath Jawahar <ragunath@redgreen.io>
2021-03-29 12:23:14 +0530

    test: gilded rose tests

---
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -0,0 +1,57 @@
+package com.gildedrose
+
+import net.jqwik.api.Arbitraries
+import net.jqwik.api.Arbitrary
+import net.jqwik.api.ForAll
+import net.jqwik.api.From
+import net.jqwik.api.Property
+import net.jqwik.api.Provide
+import org.approvaltests.Approvals
+
+class GildedRoseTest {
+  companion object {
+    const val DATA_SET_SIZE = 50
+  }
+
+  @Property(
+    tries = 1,
+    seed = "-8556729318537930054"
+  )
+  fun `aged brie`(
+    @ForAll @From("sellIns") sellIns: List<Int>,
+    @ForAll @From("qualities") qualities: List<Int>,
+  ) {
+    // given
+    val name = "Aged Brie"
+    val items = sellIns.zip(qualities)
+      .map { (sellIn, quality) -> Item(name, sellIn, quality) }
+    val gildedRose = GildedRose(items.toTypedArray())
+
+    // when
+    gildedRose.updateQuality()
+
+    // then
+    val gildedRoseSnapshot = gildedRose
+      .items
+      .joinToString("\n") { it.toString() }
+    Approvals.verify(gildedRoseSnapshot)
+  }
+
+  @Provide
+  fun sellIns(): Arbitrary<List<Int>> =
+    Arbitraries
+      .integers()
+      .between(0, 50)
+      .list()
+      .uniqueElements()
+      .ofSize(DATA_SET_SIZE)
+
+  @Provide
+  fun qualities(): Arbitrary<List<Int>> =
+    Arbitraries
+      .integers()
+      .between(-10, 60)
+      .list()
+      .uniqueElements()
+      .ofSize(DATA_SET_SIZE)
+}
