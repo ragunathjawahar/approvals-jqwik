45fef41983cfbb4c0d89bda22f991fba5567de06
Ragunath Jawahar <ragunath@redgreen.io>
2021-03-29 12:29:24 +0530

    test: add tests for each item

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -17,12 +17,84 @@ class GildedRoseTest {
     tries = 1,
     seed = "-8556729318537930054"
   )
-  fun `aged brie`(
+  fun `Aged Brie`(
     @ForAll @From("sellIns") sellIns: List<Int>,
     @ForAll @From("qualities") qualities: List<Int>,
   ) {
     // given
     val name = "Aged Brie"
+    val items = sellIns.zip(qualities)
+      .map { (sellIn, quality) -> Item(name, sellIn, quality) }
+    val gildedRose = GildedRose(items.toTypedArray())
+
+    // when
+    gildedRose.updateQuality()
+
+    // then
+    val gildedRoseSnapshot = gildedRose
+      .items
+      .joinToString("\n") { it.toString() }
+    Approvals.verify(gildedRoseSnapshot)
+  }
+
+  @Property(
+    tries = 1,
+    seed = "1002985723690070170"
+  )
+  fun `Backstage passes to a TAFKAL80ETC concert`(
+    @ForAll @From("sellIns") sellIns: List<Int>,
+    @ForAll @From("qualities") qualities: List<Int>,
+  ) {
+    // given
+    val name = "Backstage passes to a TAFKAL80ETC concert"
+    val items = sellIns.zip(qualities)
+      .map { (sellIn, quality) -> Item(name, sellIn, quality) }
+    val gildedRose = GildedRose(items.toTypedArray())
+
+    // when
+    gildedRose.updateQuality()
+
+    // then
+    val gildedRoseSnapshot = gildedRose
+      .items
+      .joinToString("\n") { it.toString() }
+    Approvals.verify(gildedRoseSnapshot)
+  }
+
+  @Property(
+    tries = 1,
+    seed = "3398327011427988732"
+  )
+  fun `Sulfuras, Hand of Ragnaros`(
+    @ForAll @From("sellIns") sellIns: List<Int>,
+    @ForAll @From("qualities") qualities: List<Int>,
+  ) {
+    // given
+    val name = "Sulfuras, Hand of Ragnaros"
+    val items = sellIns.zip(qualities)
+      .map { (sellIn, quality) -> Item(name, sellIn, quality) }
+    val gildedRose = GildedRose(items.toTypedArray())
+
+    // when
+    gildedRose.updateQuality()
+
+    // then
+    val gildedRoseSnapshot = gildedRose
+      .items
+      .joinToString("\n") { it.toString() }
+    Approvals.verify(gildedRoseSnapshot)
+  }
+
+  @Property(
+    tries = 1,
+    seed = "6350692656906137395"
+  )
+  fun `all other items`(
+    @ForAll @From("sellIns") sellIns: List<Int>,
+    @ForAll @From("qualities") qualities: List<Int>,
+  ) {
+    // given
+    val name = "all other items"
     val items = sellIns.zip(qualities)
       .map { (sellIn, quality) -> Item(name, sellIn, quality) }
     val gildedRose = GildedRose(items.toTypedArray())
